e05ff9a45b25f4f33000ae8ff83c199b3f4f106c
fryntiz <tecnico@fryntiz.es>
2017-09-19 12:59:02 +0200

    Planteando base del script

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+# Buscar el archivo publicar.ods y si no existe pedir por pantalla la ruta absoluta y nombre del archivo
+def buscarODS()
+	print('Buscando archivo ODS')
+
+# Probar abrir el archivo y si da error volver a la función anterior
+
+# Conectar a la API
