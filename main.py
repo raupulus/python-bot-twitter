0626f28b44231ea9959c9cf97a2ac96e72d2b861
fryntiz <tecnico@fryntiz.es>
2017-09-19 13:00:08 +0200

    Cabecera del script

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
 
+#######################################
+# ###     Raúl Caro Pastorino     ### #
+## ##                             ## ##
+### # https://github.com/fryntiz/ # ###
+## ##                             ## ##
+# ###       www.fryntiz.es        ### #
+#######################################
+
+##############################
+##    Importar Librerías    ##
+##############################
+
+import time #Importamos la libreria time --> time.sleep
+import os #Importamos la libreria para comandos de la consola/shell/bash/sistema
+import random #Genera números aleatorios --> random.randrange(1,100)
+
+##############################
+##         Variables        ##
+##############################
+
+sleep = time.sleep #variable para usar con más comodidad el control de tiempo
 
 
 
