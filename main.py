48aa73ca432ba48e3a3ee414e419e8c5581b1344
fryntiz <tecnico@fryntiz.es>
2017-09-20 12:31:35 +0200

    Planteada función para comprobar si existe archivo

--- main.py
+++ main.py
@@ -16,14 +16,26 @@
 import time #Importamos la libreria time --> time.sleep
 import os #Importamos la libreria para comandos de la consola/shell/bash/sistema
 import random #Genera números aleatorios --> random.randrange(1,100)
+import os.path #Se utilizará para comprobar que existe un archivo --> os.path.isfile(fname) y os.path.islink(fname)
 
+import ODS_to_CSV
+import VAR.lst #importar lista de variables
 ##############################
 ##         Variables        ##
 ##############################
-
 sleep = time.sleep #variable para usar con más comodidad el control de tiempo
 
+#Función a la que se pasa un nombre o ruta hacia archivo y devuelve booleano
+def existe_archivo(nombre)
+	os.path.isfile(nombre)
 
+print('Buscando archivo \"Publicar.ods\"')
+if existe_archivo(Publicar.ods)
+	print('Utilizando el Archivo Publicar.ods de este mismo directorio')
+else
+	ARCHIVO_ENTRADA = raw_input('Introduce la ruta completa hasta el archivo')
+	#TODO --> Necesario controlar que existe el archivo: Crear función en bucle hasta comprobar
+toODS(PUB_IN)
 
 
 # Buscar el archivo publicar.ods y si no existe pedir por pantalla la ruta absoluta y nombre del archivo
