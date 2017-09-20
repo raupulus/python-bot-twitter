73fc057457af6b7a6310c625d35a8840158b9587
fryntiz <tecnico@fryntiz.es>
2017-09-20 13:11:18 +0200

    Renombrada lista de variables para ser importadas más cómodas

---
+++ VAR.py
@@ -0,0 +1,16 @@
+#Patrones de retwitt separados por coma ","
+RETWITT = ''
+
+#Patrones de Twitt
+TWITT = ''
+
+#Patrones de Favorito
+FAV = ''
+
+#Datos de API
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+
+ARCHIVO_ENTRADA = 'Publicar.ods'
