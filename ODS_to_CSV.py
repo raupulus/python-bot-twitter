7ccfeadac382064f9724bee732eb5f276a3d6ec9
fryntiz <tecnico@fryntiz.es>
2017-09-20 13:14:39 +0200

    Descomentada variable con nombre de archivo

--- ODS_to_CSV.py
+++ ODS_to_CSV.py
@@ -10,12 +10,12 @@
 #######################################
 
 #Función para crear un archivo CSV a partir del ODS pasado usando solo la columna "B"
-def toODS(nombreArchivo)
+def toODS(nombreArchivo):
 	import pyexcel as pe
 	import codecs #Librería para codificar en UTF-8 (Error al write ñ)
 
 	#Solo extraer la columna "B" de cada hoja
-	HOJAS = pe.get_book(file_name="nombreArchivo", start_column=1, column_limit=1)
+	HOJAS = pe.get_book(file_name=nombreArchivo, start_column=1, column_limit=1)
 
 	#Abrir archivo donde escribir
 	SALIDA = codecs.open('Publicar.csv', 'w', encoding='utf8')
