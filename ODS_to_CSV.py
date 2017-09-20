2adbe4d3775f9901788f4694cb0f392fc6e18744
fryntiz <tecnico@fryntiz.es>
2017-09-20 12:43:54 +0200

    Variables renombradas para ser más entendibles y comentada función

--- ODS_to_CSV.py
+++ ODS_to_CSV.py
@@ -9,18 +9,19 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
+#Función para crear un archivo CSV a partir del ODS pasado usando solo la columna "B"
 def toODS(nombreArchivo)
 	import pyexcel as pe
 	import codecs #Librería para codificar en UTF-8 (Error al write ñ)
 
 	#Solo extraer la columna "B" de cada hoja
-	sheet = pe.get_book(file_name="publicar.ods", start_column=1, column_limit=1)
+	HOJAS = pe.get_book(file_name="nombreArchivo", start_column=1, column_limit=1)
 
 	#Abrir archivo donde escribir
-	salida = codecs.open('publicar.csv', 'w', encoding='utf8')
+	SALIDA = codecs.open('Publicar.csv', 'w', encoding='utf8')
 
 	#Pasar cada línea al archivo csv
-	for lines in sheet:
+	for lines in HOJAS:
 		for line in lines:
 			print('Escribiendo: ' + line[0])
-			salida.write(line[0] + '\n')
+			SALIDA.write(line[0] + '\n')
