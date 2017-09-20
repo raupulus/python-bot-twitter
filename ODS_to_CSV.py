1be8eb21bf6318482e268974df8b4a5a39845ae0
fryntiz <tecnico@fryntiz.es>
2017-09-20 10:05:40 +0200

    Optimizado funcionamiento para cumplir con su función

--- ODS_to_CSV.py
+++ ODS_to_CSV.py
@@ -9,21 +9,18 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
-import pyexcel as pe
-import codecs #Librería para codificar en UTF-8 (Error al write ñ)
+def toODS(nombreArchivo)
+	import pyexcel as pe
+	import codecs #Librería para codificar en UTF-8 (Error al write ñ)
 
-#filas --> start_row=X, row_limit=X
-#Columnas --> start_column=X, column_limit=X
-#Se fija para que solo extraiga la columna "B" así:
-sheet = pe.get_book(file_name="test.ods", start_column=1, column_limit=1)
+	#Solo extraer la columna "B" de cada hoja
+	sheet = pe.get_book(file_name="publicar.ods", start_column=1, column_limit=1)
 
-#Abrir archivo donde escribir
-salida = codecs.open('test.csv', 'w', encoding='utf8')
+	#Abrir archivo donde escribir
+	salida = codecs.open('publicar.csv', 'w', encoding='utf8')
 
-#Pasar cada línea al archivo csv
-for lines in sheet:
-	#Con la siguiente opción se crea un csv por cada hoja existente:
-	#print(lines).save_as("Hoja.csv")
-	for line in lines:
-		print('Escribiendo: ' + line[0])
-		salida.write(line[0] + '\n')
+	#Pasar cada línea al archivo csv
+	for lines in sheet:
+		for line in lines:
+			print('Escribiendo: ' + line[0])
+			salida.write(line[0] + '\n')
