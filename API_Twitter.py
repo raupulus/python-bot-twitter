2674e71011154f3b6efb255487549b3f28c9cb0a
fryntiz <tecnico@fryntiz.es>
2017-09-21 04:04:40 +0200

    Comentado y limpieza

--- API_Twitter.py
+++ API_Twitter.py
@@ -14,19 +14,18 @@
 ##Función para analizar comentarios de un tweet
 ##Función para retwittear todo lo de un array de personas
 
-#Función para conectar con la API de Twitter
+#Función para conectar con la API de Twitter (Variables en VAR.py)
 def conectar():
 	print('Conectando con la API')
 	autenticar = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 	autenticar.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 	api = tweepy.API(autenticar)
 
-#Función para Publicar en Twitter
+#Función para Publicar en Twitter (Recibe una cadena de 1 sola línea a publicar)
 def publicar(publicacion):
 	print("[+] Twitteando la siguiente entrada...")
-	api.update_status(status = line)
-	print("[+] Tweet: ", line)
-	time.sleep(5)
+	api.update_status(status = publicacion)
+	print("[+] Tweet: ", publicacion)
 
 
 
