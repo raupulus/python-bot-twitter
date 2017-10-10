acd129bb01f69ecefa91aacb810dbf763c311ba9
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-10 18:09:29 +0200

    Comprueba que la entrada tenga más de 20 carácteres y menos de 140

--- ODS_to_CSV.py
+++ ODS_to_CSV.py
@@ -11,19 +11,19 @@
 
 #Función para crear un archivo CSV a partir del ODS pasado usando solo la columna "B"
 def toODS(nombreArchivo):
-	import pyexcel as pe
-	import codecs #Librería para codificar en UTF-8 (Error al write ñ)
+    import pyexcel as pe
+    import codecs #Librería para codificar en UTF-8 (Error al write ñ)
 
-	#Solo extraer la columna "B" de cada hoja
-	HOJAS = pe.get_book(file_name=nombreArchivo, start_column=1, column_limit=1)
+    #Solo extraer la columna "B" de cada hoja
+    HOJAS = pe.get_book(file_name=nombreArchivo, start_column=1, column_limit=1)
 
-	#Abrir archivo donde escribir
-	SALIDA = codecs.open('Publicar.csv', 'w', encoding='utf8')
+    #Abrir archivo donde escribir
+    SALIDA = codecs.open('Publicar.csv', 'w', encoding='utf8')
 
-	##TOFIX
-	#Pasar cada línea al archivo csv
-	for lines in HOJAS:
-		for line in lines:
-			#Comprobar que la línea no está en blanco (if -- continue)
-			#Comprobar que la línea tiene más de 10 carácteres (elif -- continue)
-			SALIDA.write(line[0] + '\n')
+    ##TOFIX
+    #Pasar cada línea al archivo csv
+    for lines in HOJAS:
+        for line in lines:
+            # Si no está vacía, debe tener más de 20 carácteres y menos de 140
+            if ((len(line[0]) > 20) and (len(line[0]) <= 140)):
+                SALIDA.write(line[0].strip() + '\n')
