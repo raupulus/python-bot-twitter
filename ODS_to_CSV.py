7895962d30b33eb8d4f139f613789f90a0c6e52a
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-13 13:05:04 +0200

    Reestructuradas funciones para convertir desde ODS

--- ODS_to_CSV.py
+++ ODS_to_CSV.py
@@ -9,15 +9,18 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
+columna_inicio = 1  # Empieza en columna B
+columna_limite = 1  # Solo lee 1 columna
+
 
 #Función para crear un archivo CSV a partir del ODS
 #Se usará solo la columna "B" del archivo pasado (por ahora)
-def toODS(nombreArchivo):
+def toCSV(ruta_archivo):
     import pyexcel as pe
     import codecs  # Librería para codificar en UTF-8 (Error al write ñ)
 
     #Solo extraer la columna "B" de cada hoja
-    HOJAS = pe.get_book(file_name=nombreArchivo, start_column=1, column_limit=1)
+    HOJAS = pe.get_book(file_name=ruta_archivo, start_column=1, column_limit=1)
 
     #Abrir archivo donde escribir
     SALIDA = codecs.open('Publicar.csv', 'w', encoding='utf8')
@@ -29,3 +32,11 @@ def toODS(nombreArchivo):
             # Si no está vacía, debe tener más de 20 carácteres y menos de 140
             if ((len(line[0]) > 20) and (len(line[0]) <= 140)):
                 SALIDA.write(line[0].strip() + '\n')
+
+
+def toBD(ruta_archivo):
+    print('Añadiendo a la BD')
+
+
+def toTXT(ruta_archivo):
+    print('Creando Publicar.txt')
