ce66644613a38a322ea278c0d1880f304852e07c
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-14 18:40:56 +0200

    Simplificado main.py en muchos aspectos

--- main.py
+++ main.py
@@ -1,49 +1,66 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
-#################################################
-# ###                                     Raúl Caro Pastorino                                     ### #
-## ##                                                                                                         ## ##
-### #                                 https://github.com/fryntiz/                                # ###
-## ##                                                                                                         ## ##
-# ###                                         www.fryntiz.es                                         ### #
-#################################################
+#######################################
+# ###     Raúl Caro Pastorino     ### #
+## ##                             ## ##
+### # https://github.com/fryntiz/ # ###
+## ##                             ## ##
+# ###       www.fryntiz.es        ### #
+#######################################
 
 # Este script se está planteando para muchos perfiles distintos pero se bloqueará
 # hasta llegar a un punto más avanzado a solo 1 perfil (posición 0 en array's)'
 
 ##############################
-##                    Importar Librerías                    ##
+##    Importar Librerías    ##
 ##############################
 import time  # Importamos la libreria time --> time.sleep
 import sys  # Importar comandos del sistema, por ejemplo exit
 import os  # Importar lib para interactuar con el sistema
 import random  # Genera números aleatorios --> random.randrange(1,100)
 import convert_ODS  # Importa de este directorio el script para convertir a CSV
+from API_TWITTER import API_TWITTER
+from publicacion import publicacion
 from VAR import *  # importar todas las variables
 
 ##############################
 ##         Variables        ##
 ##############################
 sleep = time.sleep  # variable para usar con más comodidad el control de tiempo
+PERFILES = ''
+ENTRADAS = ''
 
 
 # Array con cada objeto perfil
-PERFILES[0] = API_TWITTER(0, "perfil1", "asd", "asd", "asd", "asd")
+def crear_perfiles():
+    global PERFILES
+    PERFILES = [API_TWITTER(0, "perfil1", "asd", "asd", "asd", "asd")]
+crear_perfiles()
+
+
+#Función a la que se pasa un nombre o ruta hacia archivo y devuelve booleano
+def existe_archivo(ruta_archivo):
+    return os.path.isfile(ruta_archivo)  # Comprobar que existe
 
 #Convertir a CSV el archivo ODS. Por defecto busca "Publicar.ods"
 def inicializar():
+    global ENTRADAS
     print('\n[+]Buscando archivo → Publicar.ods')
-    if existe_archivo(Publicar.ods):
+    if existe_archivo('Publicar.ods'):
         print('[+]Utilizando el Archivo Publicar.ods')
         # Convertir ODS en CSV
         convert_ODS.toCSV("Publicar.ods")
-    else:
-        print('[~]Archivo Publicar.ods no encontrado')
-        ARCHIVO_ENTRADA = raw_input('Ruta completa hasta el archivo: ')
+    # TOFIX → Si no existe Publicar.ods se almacena en "ARCHIVO_ENTRADA"
+    # pero no es del todo claro. Plantear dar aviso e ignorar perfil y publicns
+    #else:
+    #    print('[~]Archivo Publicar.ods no encontrado')
+    #    ARCHIVO_ENTRADA = raw_input('Ruta completa hasta el archivo: ')
 
-    # Array con cada entradas. La posición coincide con posición en PERFILES
-    ENTRADAS[0] = publicacion("Publicar.csv")
+    #Se comprueba que el CSV se ha creado antes de intentar crear objetos
+    if existe_archivo('Publicar.csv'):
+        # Array con cada entrada. La posición coincide con posición en PERFILES
+        ENTRADAS = [publicacion("Publicar.csv")]
 inicializar()
 
 
@@ -90,7 +107,8 @@ def seguir():
 def test0():
     #Bucle temporal para crear la cadena a publicar a partir de la línea
     while True:
-        print ('[+] Entrada ')
+        print('\n[+] Entrada Publicada:\n' + ENTRADAS[0].publicacion_actual())
+        ENTRADAS[0].siguiente_linea()
         sleep(5)
 test0()
 
@@ -110,6 +128,13 @@ def test2():
         sleep(21600)  # 6 Horas entre publicaciones
 #test2
 
+#Obtiene el timeline agregándolo a un archivo
+def test3():
+    print('Conectando API')
+    print('Obteniendo timeline')
+    print('Pasándolo al archivo XXXX')
+#test3
+
 
 
 #Twittear 1 entrada cada X minutos (2 en total)
@@ -118,7 +143,7 @@ def test2():
 
 #Retwittear 1 twitt cada X minutos (5 en total)
 
-#Estructura para controlar el tiempo total que trabajará el bot (o hasta infinito)
+#Estructura para controlar el tiempo total que trabajará el bot (o infinito)
 
 #Preparando para cerrar
 #print('\n[+]Cerrando Script')
