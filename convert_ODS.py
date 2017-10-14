4df51b080f4a1e8b8dbdac6f4e1dffb1225be4f1
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-14 18:40:30 +0200

    Reestructurada la opción de convertir ODS a otros formatos

--- convert_ODS.py
+++ convert_ODS.py
@@ -11,6 +11,8 @@
 
 import pyexcel as pe
 import codecs  # Librería para codificar en UTF-8 (Error al write ñ)
+import os  # Importar lib para interactuar con el sistema
+
 
 columna_inicio = 1  # Empieza en columna B
 columna_limite = 1  # Solo lee 1 columna
@@ -24,7 +26,7 @@ def existe_archivo(ruta_archivo):
 #Función para crear un archivo CSV a partir del ODS
 #Se usará solo la columna "B" del archivo pasado (por ahora)
 def toCSV(ruta_archivo):
-    global columna_inicio, columna_limite
+    columna_inicio, columna_limite
     if existe_archivo(ruta_archivo):
         #Solo extraer la columna "B" de cada hoja
         HOJAS = pe.get_book(
@@ -38,7 +40,7 @@ def toCSV(ruta_archivo):
         #Pasar cada línea al archivo csv
         for lines in HOJAS:
             for line in lines:
-                # Si no está vacía, debe tener más de 20 carácteres y menos de 140
+                # Debe tener más de 20 carácteres y menos de 140
                 if ((len(line[0]) > 20) and (len(line[0]) <= 140)):
                     SALIDA_CSV.write(line[0].strip() + '\n')
 
