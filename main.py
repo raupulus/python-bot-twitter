b3e716924e94e68285d8a03f42645365cd2a932f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-10-17 01:33:03 +0200

    Construida función para leer TOKEn

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from publicacion import publicacion
 ##         Variables        ##
 ##############################
 sleep = time.sleep  # variable para usar con más comodidad el control de tiempo
-PERFILES = ''
+PERFILES = []
 CANTIDAD_PERFILES = 0
 ENTRADAS = ''
 
@@ -34,33 +34,44 @@ def crear_perfiles():
     global PERFILES, CANTIDAD_PERFILES
     listar_perfiles = os.listdir("Perfiles")
     contador_id = 0
+    ACCESS_KEY = ''
+    ACCESS_SECRET = ''
+    CONSUMER_KEY = ''
+    CONSUMER_SECRET = ''
 
     #Crear un objeto Perfil con los datos de cada subdirectorio "Perfil"
     for perfil in listar_perfiles:
         if ((perfil != 'Plantilla') and (perfil != 'plantilla') and
             (perfil != 'Plantillas') and (perfil != 'plantillas')):
 
-            #Leer en "Perfiles/" + perfil + "/TOKEN.csv" los valores de API
-            #TODO → Extraer de TOKEN.csv los valores:
-            ACCESS_KEY = ''
-            ACCESS_SECRET = ''
-            CONSUMER_KEY = ''
-            CONSUMER_SECRET = ''
+            #Leer en "Perfiles/" + perfil + "/token.csv" los valores de API
+            tmp_token = open('./Perfiles/' + perfil + '/token.csv', 'r')
+            for line in tmp_token:
+                line_clean = line.replace(' ', '').strip().split('=')
+                if (line_clean[0].upper() == 'ACCESS_KEY'):
+                    print('ACCESS_KEY → ' + line_clean[1])
+                    ACCESS_KEY = line_clean[1]
+                elif (line_clean[0].upper() == 'ACCESS_SECRET'):
+                    print('ACCESS_KEY → ' + line_clean[1])
+                    ACCESS_KEY = line_clean[1]
+                elif (line_clean[0].upper() == 'CONSUMER_KEY'):
+                    print('ACCESS_KEY → ' + line_clean[1])
+                    ACCESS_KEY = line_clean[1]
+                elif (line_clean[0].upper() == 'CONSUMER_SECRET'):
+                    print('ACCESS_KEY → ' + line_clean[1])
+                    ACCESS_KEY = line_clean[1]
 
-            print('Creando el perfil → ' + perfil)
 
+            #TOFIX → Solo añade el último perfil
             #Creando objeto "perfil(id,nombre,AK,AS,CK,CS"
-            PERFILES = [API_TWITTER(contador_id, perfil,
-                ACCESS_KEY, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)]
+            print('Creando perfil: ' + perfil + ' id-' + str(contador_id))
+            PERFILES.append(API_TWITTER(contador_id, perfil,
+                ACCESS_KEY, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
             contador_id += 1
 
     #Recuenta la cantidad total de perfiles
     CANTIDAD_PERFILES = len(PERFILES)
     print('\n[+]Cantidad de perfiles → ' + str(CANTIDAD_PERFILES))
-
-
-    #Leer archivo TOKEN.csv y pasar los parámetros al constructor
-    #Aumentar contador de id en constructor
 
 # Crea el array de entradas para cada perfil
 #TOFIX → Plantear que esto lo haga la clase perfil (API_TWITTER)
