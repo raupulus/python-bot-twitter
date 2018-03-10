b11e9a78dcda52daa9fdfcc41a57d280432cd210
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-03-10 03:13:20 +0100

    Creado archivo para contener datos de token

---
+++ token.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+#######################################
+# ###     Raúl Caro Pastorino     ### #
+## ##                             ## ##
+### # https://github.com/fryntiz/ # ###
+## ##                             ## ##
+# ###       www.fryntiz.es        ### #
+#######################################
